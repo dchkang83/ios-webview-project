1c9cebe4e7dc69f329f864cb24b3c9477cfbd6bc
deokjoon.kang <dchkang83@gmail.com>
2023-01-10 18:30:55 +0900

    IOS/WebView/Project - View 이동 테스트 까지 완료... STEP 별로.. 커밋 안남겻네 OTL..

---
+++ middlework/Controllers/InitViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InitViewController.swift
+//  Storyboard1
+//
+//  Created by 강덕준 on 1/10/23.
+//
+
+import Foundation
+import UIKit
+
+class InitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("######### Init ViewController - viewDidLoad")
+    }
+}
---
+++ middlework/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Storyboard1
+//
+//  Created by 강덕준 on 1/10/23.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("######### Main ViewController - viewDidLoad")
+    }
+}
